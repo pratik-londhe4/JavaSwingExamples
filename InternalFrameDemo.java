44f77059c0b77665badcbb405475f2f8d5793d66
pratik-londhe4 <pratik.londhe@moderncoe.edu.in>
2022-02-11 15:29:05 +0530

    initial commit

---
+++ InternalFrameDemo.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+class MyFrame7 extends JFrame{
+
+
+
+}
+
+public class InternalFrameDemo {
+    public static void main(String[] args) {
+        MyFrame7 mf = new MyFrame7();
+        mf.setSize(800 , 800);
+        mf.setVisible(true);
+        mf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
