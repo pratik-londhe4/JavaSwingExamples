15cf6e35f3e9389f4d909b3b741cfb7c94885f37
pratik-londhe4 <pratik.londhe@moderncoe.edu.in>
2022-02-11 15:51:29 +0530

    completed internal frame demo

--- InternalFrameDemo.java
+++ InternalFrameDemo.java
@@ -1,12 +1,54 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 class MyInternalFrame extends JInternalFrame{
-    
+    JTextArea ta;
+    static  int count = 0;
+    JScrollPane sp;
+
+    MyInternalFrame(){
+        super("Document "+ (++count) , true , true , true , true );
+        ta = new JTextArea();
+        sp = new JScrollPane(ta);
+        add(sp);
+
+        JMenuBar mb = new JMenuBar();
+        JMenu file = new JMenu("File");
+        JMenuItem m1 = new JMenuItem("Save");
+        file.add(m1);
+        mb.add(file);
+        setJMenuBar(mb);
+        setSize(300 , 300);
+        setLocation(50 , 50);
+        setVisible(true);
+    }
+
 }
-class MyFrame7 extends JFrame{
+class MyFrame7 extends JFrame implements ActionListener {
+
+    JDesktopPane jp;
+
+    MyFrame7(){
+        super("Internal Frame Demo");
+        jp = new JDesktopPane();
+        setContentPane(jp);
+        JMenuBar mb = new JMenuBar();
+        JMenu d = new JMenu("Document");
+        JMenuItem m1 = new JMenuItem("New");
+        d.add(m1);
+        mb.add(d);
+        setJMenuBar(mb);
+
+        m1.addActionListener(this);
+    }
 
 
-
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        MyInternalFrame mi = new MyInternalFrame();
+        jp.add(mi);
+    }
 }
 
 public class InternalFrameDemo {
